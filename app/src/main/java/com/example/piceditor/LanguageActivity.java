9414baee08aa8199e66566983335588b1131661a
duyhieuday <89791695+duyhieuday@users.noreply.github.com>
2026-03-31 17:30:45 +0700

    3/31 update ui

---
+++ app/src/main/java/com/example/piceditor/LanguageActivity.java
@@ -0,0 +1,457 @@
+package com.example.piceditor;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.adjust.sdk.Adjust;
+import com.adjust.sdk.AdjustAdRevenue;
+import com.adjust.sdk.AdjustConfig;
+import com.example.piceditor.base.BaseActivityNew;
+import com.example.piceditor.databinding.ActivityLanguageBinding;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdValue;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.OnPaidEventListener;
+import com.google.android.gms.ads.VideoController;
+import com.google.android.gms.ads.VideoOptions;
+import com.google.android.gms.ads.nativead.NativeAd;
+import com.google.android.gms.ads.nativead.NativeAdOptions;
+import com.google.android.gms.ads.nativead.NativeAdView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Locale;
+
+public class LanguageActivity extends BaseActivityNew<ActivityLanguageBinding> {
+
+    private LanguageAdapter languageAdapter;
+    private List<Language> languageList;
+    private SharedPreferences sharedPreferences;
+    private NativeAd currentNativeAd;
+    private NativeAd currentNativeAd2;
+    private boolean isNative = true;
+    int isPro = new com.changeicon.theme.ads.Prefs(WeatherApplication.get()).getPremium();
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.activity_language;
+    }
+
+    @Override
+    public int getFrame() {
+        return 0;
+    }
+
+    @Override
+    public void getDataFromIntent() {
+
+    }
+
+    @Override
+    public void doAfterOnCreate() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public BaseFragment initFragment() {
+        return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        BarsUtils.setStatusBarColor(this, Color.parseColor("#01000000"));
+        BarsUtils.setAppearanceLightStatusBars(this, true);
+
+        Gson gson = new Gson();
+        Type language = new TypeToken<List<Language>>() {
+        }.getType();
+        List<Language> languages = null;
+        try {
+            languages = gson.fromJson(new InputStreamReader(this.getAssets().open("data/languages.json")), language);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        getBinding().rcvLanguage.setLayoutManager(new LinearLayoutManager(
+                this, RecyclerView.VERTICAL, false
+        ));
+
+        languageList = languages;
+        languageAdapter = new LanguageAdapter();
+        languageAdapter.setData(languageList);
+
+        getBinding().rcvLanguage.setAdapter(languageAdapter);
+        getBinding().rcvLanguage.smoothScrollToPosition(0);
+
+        Log.e("cxcc", "languages: " + languages );
+        Log.e("cxcc", "languageList: " + languageList );
+
+        languageAdapter.setLanguageListener(new LanguageListener() {
+            @Override
+            public void onLanguageClick(int position, Language language) {
+                Prefs.putString(Prefs.Key.LANGUAGE_NAME, language.getLanguageName());
+                sharedPreferences = getSharedPreferences("signLanguage", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("getSignLanguage", language.getSignLanguage());
+                editor.apply();
+                getBinding().iconTick.setVisibility(View.VISIBLE);
+                if (isNative) {
+                    getBinding().adFrame1.setVisibility(View.VISIBLE);
+                    isNative = false;
+                }
+            }
+        });
+
+        LanguageManager languageManager = new LanguageManager(LanguageActivity.this);
+        getBinding().iconTick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(PreferenceUtil.getInstance(LanguageActivity.this).getValue(Constant.SharePrefKey.TEST_OBD, "no").equals("yes") && PreferenceUtil.getInstance(getApplicationContext()).getValue(Constant.SharePrefKey.HEHE, false) && isPro == 0) {
+                    if (Prefs.getBoolean(Prefs.Key.FIRST_ONBOARDING, true)) {
+                        sharedPreferences = getSharedPreferences("signLanguage", MODE_PRIVATE);
+                        String signLanguage = sharedPreferences.getString("getSignLanguage", null);
+                        if (signLanguage == null) {
+                            String systemLanguage = Locale.getDefault().getLanguage();
+                            languageManager.updateResource(systemLanguage);
+                        } else {
+                            languageManager.updateResource(signLanguage);
+                        }
+                        Intent intent = new Intent(LanguageActivity.this, OnboardingActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Prefs.putBoolean(Prefs.Key.KEY_LANGUAGE, false);
+                        sharedPreferences = getSharedPreferences("signLanguage", MODE_PRIVATE);
+                        String signLanguage = sharedPreferences.getString("getSignLanguage", null);
+                        if (signLanguage == null) {
+                            String systemLanguage = Locale.getDefault().getLanguage();
+                            languageManager.updateResource(systemLanguage);
+                        } else {
+                            languageManager.updateResource(signLanguage);
+                        }
+                        Intent intent = new Intent(LanguageActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }else {
+                    Prefs.putBoolean(Prefs.Key.KEY_LANGUAGE, false);
+                    sharedPreferences = getSharedPreferences("signLanguage", MODE_PRIVATE);
+                    String signLanguage = sharedPreferences.getString("getSignLanguage", null);
+                    if (signLanguage == null) {
+                        String systemLanguage = Locale.getDefault().getLanguage();
+                        languageManager.updateResource(systemLanguage);
+                    } else {
+                        languageManager.updateResource(signLanguage);
+                    }
+                    Intent intent = new Intent(LanguageActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        });
+
+        if (isPro == 0){
+            refreshAd();
+            refreshAd1();
+        }
+
+    }
+
+    private void populateNativeAdView(NativeAd nativeAd, AdUnifiedLanguageBinding adView) {
+        NativeAdView nativeAdView = adView.rootNative;
+
+        // Set the media view.
+        nativeAdView.setMediaView(findViewById(R.id.ad_media));
+
+        // Set other ad assets.
+        nativeAdView.setHeadlineView(adView.adHeadline);
+        nativeAdView.setBodyView(adView.adBody);
+        nativeAdView.setCallToActionView(adView.adCallToAction);
+        nativeAdView.setIconView(adView.adAppIcon);
+        nativeAdView.setPriceView(adView.adPrice);
+        nativeAdView.setStarRatingView(adView.adStars);
+        nativeAdView.setStoreView(adView.adStore);
+        nativeAdView.setAdvertiserView(adView.adAdvertiser);
+
+        adView.adHeadline.setText(nativeAd.getHeadline());
+
+        // Set media content if available
+        if (nativeAd.getMediaContent() != null) {
+            adView.adMedia.setMediaContent(nativeAd.getMediaContent());
+        }
+
+        if (nativeAd.getBody() == null) {
+            adView.adBody.setVisibility(View.GONE);
+        } else {
+            adView.adBody.setVisibility(View.VISIBLE);
+            adView.adBody.setText(nativeAd.getBody());
+        }
+
+        if (nativeAd.getCallToAction() == null) {
+            adView.adCallToAction.setVisibility(View.INVISIBLE);
+        } else {
+            adView.adCallToAction.setVisibility(View.VISIBLE);
+            adView.adCallToAction.setText(nativeAd.getCallToAction());
+        }
+
+        if (nativeAd.getIcon() == null) {
+            adView.adAppIcon.setVisibility(View.GONE);
+        } else {
+            adView.adAppIcon.setImageDrawable(nativeAd.getIcon().getDrawable());
+            adView.adAppIcon.setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getPrice() == null) {
+            adView.adPrice.setVisibility(View.GONE);
+        } else {
+            adView.adPrice.setVisibility(View.GONE);
+            adView.adPrice.setText(nativeAd.getPrice());
+        }
+
+        if (nativeAd.getStore() == null) {
+            adView.adStore.setVisibility(View.GONE);
+        } else {
+            adView.adStore.setVisibility(View.GONE);
+            adView.adStore.setText(nativeAd.getStore());
+        }
+
+// Set star rating visibility and value
+        if (nativeAd.getStarRating() == null) {
+            adView.adStars.setVisibility(View.GONE);
+        } else {
+            adView.adStars.setRating(nativeAd.getStarRating().floatValue());
+            adView.adStars.setVisibility(View.GONE);
+        }
+
+// Set advertiser visibility and text
+        if (nativeAd.getAdvertiser() == null) {
+            adView.adAdvertiser.setVisibility(View.GONE);
+        } else {
+            adView.adAdvertiser.setText(nativeAd.getAdvertiser());
+            adView.adAdvertiser.setVisibility(View.VISIBLE);
+        }
+
+        nativeAdView.setNativeAd(nativeAd);
+
+        VideoController vc = nativeAd.getMediaContent().getVideoController();
+        if (vc != null && vc.hasVideoContent()) {
+            vc.setVideoLifecycleCallbacks(new VideoController.VideoLifecycleCallbacks() {
+                @Override
+                public void onVideoEnd() {
+                    super.onVideoEnd();
+                }
+            });
+        }
+    }
+
+    private void refreshAd() {
+        if (SplashActivity.currentNativeAd != null && !isDestroyed() && !isFinishing() && !isChangingConfigurations()) {
+            AdUnifiedLanguageBinding unifiedAdBinding = AdUnifiedLanguageBinding.inflate(getLayoutInflater());
+            populateNativeAdView(SplashActivity.currentNativeAd, unifiedAdBinding);
+            getBinding().adFrame.removeAllViews();
+            getBinding().adFrame.addView(unifiedAdBinding.getRoot());
+            return;
+        }
+
+        AdLoader.Builder builder = new AdLoader.Builder(this, BuildConfig.DEBUG ? IdAds.ADMOB_AD_UNIT_ID_TEST : IdAds.NATIVE_LANGUAGE1_ID);
+
+        builder.forNativeAd(new com.google.android.gms.ads.nativead.NativeAd.OnNativeAdLoadedListener() {
+            @Override
+            public void onNativeAdLoaded(@NonNull NativeAd nativeAd) {
+                try {
+                    if (!isDestroyed() && !isFinishing() && !isChangingConfigurations()) {
+                        nativeAd.setOnPaidEventListener(new OnPaidEventListener() {
+                            @Override
+                            public void onPaidEvent(@NonNull AdValue adValue) {
+                                try {
+                                    WeatherApplication.initROAS(adValue.getValueMicros(), adValue.getCurrencyCode());
+                                    AdjustAdRevenue adRevenue = new AdjustAdRevenue(AdjustConfig.AD_REVENUE_ADMOB);
+                                    adRevenue.setRevenue((double) (adValue.getValueMicros() / 1000000f), adValue.getCurrencyCode());
+                                    Adjust.trackAdRevenue(adRevenue);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+
+                        currentNativeAd = nativeAd;
+                        AdUnifiedLanguageBinding unifiedAdBinding = AdUnifiedLanguageBinding.inflate(getLayoutInflater());
+                        populateNativeAdView(nativeAd, unifiedAdBinding);
+                        getBinding().adFrame.removeAllViews();
+                        getBinding().adFrame.addView(unifiedAdBinding.getRoot());
+                    } else {
+                        nativeAd.destroy();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        VideoOptions videoOptions = new VideoOptions.Builder().setStartMuted(true).build();
+        NativeAdOptions adOptions = new NativeAdOptions.Builder().setVideoOptions(videoOptions).build();
+        builder.withNativeAdOptions(adOptions);
+
+        AdLoader adLoader = builder.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                AdError adError = loadAdError.getCause();
+                String error = "Domain: " + loadAdError.getDomain() + ", Code: " + loadAdError.getCode() + ", Message: " + loadAdError.getMessage();
+            }
+        }).build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+
+    private void populateNativeAdView1(NativeAd nativeAd, AdUnifiedOnboardingBinding adView) {
+        NativeAdView nativeAdView = adView.rootNative;
+
+        // Set the media view.
+        nativeAdView.setMediaView(nativeAdView.findViewById(R.id.ad_media));
+
+        // Set other ad assets.
+        nativeAdView.setHeadlineView(adView.adHeadline);
+        nativeAdView.setBodyView(adView.adBody);
+        nativeAdView.setCallToActionView(adView.adCallToAction);
+        nativeAdView.setIconView(adView.adAppIcon);
+        nativeAdView.setStarRatingView(adView.adStars);
+        nativeAdView.setAdvertiserView(adView.adAdvertiser);
+        nativeAdView.setMediaView(adView.adMedia);
+
+        adView.adHeadline.setText(nativeAd.getHeadline());
+
+        // Set media content if available
+        if (nativeAd.getMediaContent() != null) {
+            adView.adMedia.setMediaContent(nativeAd.getMediaContent());
+        }
+
+        if (nativeAd.getBody() == null) {
+            adView.adBody.setVisibility(View.GONE);
+        } else {
+            adView.adBody.setVisibility(View.VISIBLE);
+            adView.adBody.setText(nativeAd.getBody());
+        }
+
+        if (nativeAd.getCallToAction() == null) {
+            adView.adCallToAction.setVisibility(View.INVISIBLE);
+        } else {
+            adView.adCallToAction.setVisibility(View.VISIBLE);
+            adView.adCallToAction.setText(nativeAd.getCallToAction());
+        }
+
+        if (nativeAd.getIcon() == null) {
+            adView.adAppIcon.setVisibility(View.GONE);
+        } else {
+            adView.adAppIcon.setImageDrawable(nativeAd.getIcon().getDrawable());
+            adView.adAppIcon.setVisibility(View.VISIBLE);
+        }
+
+// Set star rating visibility and value
+        if (nativeAd.getStarRating() == null) {
+            adView.adStars.setVisibility(View.GONE);
+        } else {
+            adView.adStars.setRating(nativeAd.getStarRating().floatValue());
+            adView.adStars.setVisibility(View.GONE);
+        }
+
+// Set advertiser visibility and text
+        if (nativeAd.getAdvertiser() == null) {
+            adView.adAdvertiser.setVisibility(View.GONE);
+        } else {
+            adView.adAdvertiser.setText(nativeAd.getAdvertiser());
+            adView.adAdvertiser.setVisibility(View.VISIBLE);
+        }
+
+        nativeAdView.setNativeAd(nativeAd);
+
+        VideoController vc = nativeAd.getMediaContent().getVideoController();
+        if (vc != null && vc.hasVideoContent()) {
+            vc.setVideoLifecycleCallbacks(new VideoController.VideoLifecycleCallbacks() {
+                @Override
+                public void onVideoEnd() {
+                    super.onVideoEnd();
+                }
+            });
+        }
+    }
+
+    private void refreshAd1() {
+
+        AdLoader.Builder builder = new AdLoader.Builder(LanguageActivity.this, BuildConfig.DEBUG ? IdAds.ADMOB_AD_UNIT_ID_TEST : IdAds.NATIVE_LANGUAGE2_ID);
+
+        builder.forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+            @Override
+            public void onNativeAdLoaded(@NonNull NativeAd nativeAd) {
+                try {
+                    if (!isDestroyed() && !isFinishing() && !isChangingConfigurations()) {
+                        nativeAd.setOnPaidEventListener(new OnPaidEventListener() {
+                            @Override
+                            public void onPaidEvent(@NonNull AdValue adValue) {
+                                try {
+                                    WeatherApplication.initROAS(adValue.getValueMicros(), adValue.getCurrencyCode());
+                                    AdjustAdRevenue adRevenue = new AdjustAdRevenue(AdjustConfig.AD_REVENUE_ADMOB);
+                                    adRevenue.setRevenue((double) (adValue.getValueMicros() / 1000000f), adValue.getCurrencyCode());
+                                    Adjust.trackAdRevenue(adRevenue);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+
+                        currentNativeAd2 = nativeAd;
+                        AdUnifiedOnboardingBinding unifiedAdBinding = AdUnifiedOnboardingBinding.inflate(getLayoutInflater());
+                        populateNativeAdView1(nativeAd, unifiedAdBinding);
+                        getBinding().adFrame1.removeAllViews();
+                        getBinding().adFrame1.addView(unifiedAdBinding.getRoot());
+
+                        Log.e("xnncs1sa", "Show native2 successfully");
+                    } else {
+                        nativeAd.destroy();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        VideoOptions videoOptions = new VideoOptions.Builder().setStartMuted(true).build();
+        NativeAdOptions adOptions = new NativeAdOptions.Builder().setVideoOptions(videoOptions).build();
+        builder.withNativeAdOptions(adOptions);
+
+        AdLoader adLoader = builder.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                AdError adError = loadAdError.getCause();
+                String error = "Domain: " + loadAdError.getDomain() + ", Code: " + loadAdError.getCode() + ", Message: " + loadAdError.getMessage();
+                Log.e("xnncs1sa", "Show native2 fail");
+            }
+        }).build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+
+    }
+
+}
---
+++ app/src/main/java/com/example/piceditor/SettingActivity.java
@@ -0,0 +1,183 @@
+package com.example.piceditor;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.piceditor.ads.Callback;
+import com.example.piceditor.ads.InterAds;
+import com.example.piceditor.base.BaseActivityNew;
+import com.example.piceditor.base.BaseFragment;
+import com.example.piceditor.databinding.ActivitySettingBinding;
+import com.example.piceditor.utilsApp.BarsUtils;
+import com.example.piceditor.utilsApp.Prefs;
+
+import java.util.Locale;
+
+public class SettingActivity extends BaseActivityNew<ActivitySettingBinding> {
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.activity_setting;
+    }
+
+    @Override
+    public int getFrame() {
+        return 0;
+    }
+
+    @Override
+    public void getDataFromIntent() {
+
+    }
+
+    @Override
+    public void doAfterOnCreate() {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public BaseFragment initFragment() {
+        return null;
+    }
+
+    @Override
+    public void afterSetContentView() {
+        super.afterSetContentView();
+        BarsUtils.setStatusBarColor(this, Color.parseColor("#01000000"));
+        BarsUtils.setAppearanceLightStatusBars(this, true);
+        BarsUtils.setHideNavigation(SettingActivity.this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getBinding().icBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InterAds.showAdsBreak(SettingActivity.this, () -> finish());
+            }
+        });
+
+        getBinding().layoutLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InterAds.showAdsBreak(SettingActivity.this, new Callback() {
+                    @Override
+                    public void callback() {
+//                        Intent intent = new Intent(SettingActivity.this, HanLanguageActivity.class);
+//                        startActivity(intent);
+//                        finish();
+                    }
+                });
+            }
+        });
+
+        getBinding().layoutFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                feedBack();
+            }
+        });
+
+        getBinding().layoutShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareApp();
+            }
+        });
+
+        getBinding().layoutRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rateApp();
+            }
+        });
+
+        getBinding().layoutPolicy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String url = "https://privacyterms.io/app/?preview=FGIE3GP5-d9pFJx30-B18qxD";
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent); // Start the intent to open the link
+                    }
+                } catch (Exception e) {
+                }
+            }
+        });
+
+        String s = Prefs.getString(Prefs.Key.LANGUAGE_NAME, "");
+        if (s.isEmpty()) {
+            String defaultLanguage = Locale.getDefault().getDisplayLanguage();
+            getBinding().textLanguage.setText(defaultLanguage);
+        } else {
+            getBinding().textLanguage.setText(s);
+        }
+
+    }
+
+    private void rateApp() {
+        try {
+            String packageName = getPackageName();
+            Uri uri = Uri.parse("market://details?id=" + packageName);
+            Intent rateIntent = new Intent(Intent.ACTION_VIEW, uri);
+            if (!(getPackageManager().queryIntentActivities(rateIntent, 0).size() > 0)) {
+                Uri webUri = Uri.parse("https://play.google.com/store/apps/details?id=" + packageName);
+                rateIntent = new Intent(Intent.ACTION_VIEW, webUri);
+            }
+            startActivity(rateIntent);
+        } catch (Exception e) {
+        }
+    }
+
+    private void feedBack() {
+            Intent Email = new Intent(Intent.ACTION_SEND);
+            Email.setType("text/email");
+            Email.putExtra(Intent.EXTRA_EMAIL, new String[]{"duyhieuworks@gmail.com"});
+            Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback Photo Collage");
+            Email.putExtra(Intent.EXTRA_TEXT, "Message: ");
+
+            Email.setPackage("com.google.android.gm");
+
+            try {
+                startActivity(Email);
+            } catch (Exception e) {
+                Toast.makeText(SettingActivity.this, getString(R.string.gmail_not_install), Toast.LENGTH_SHORT).show();
+            }
+    }
+
+    private void shareApp() {
+        try {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name");
+            String shareMessage = "\nLet me recommend you this application\n\n";
+            shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+        } catch (Exception e) {
+            //e.toString();
+        }
+    }
+
+}
